1f73babd1e1e0567b004862d525a364774a19914
roryg11 <Rory.C.Grant@gmail.com>
2015-03-15 21:15:22 -0700

    beginnings of click events on Bracket

--- app/controllers/bracket.js
+++ app/controllers/bracket.js
@@ -1,9 +1,50 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  roundTwoGameOneTeamOne: "",
+  roundTwoGameOneTeamTwo: "",
+  roundTwoGameTwoTeamOne: "",
+  roundTwoGameTwoTeamTwo: "",
+  roundTwoGameThreeTeamOne: "",
+  roundTwoGameThreeTeamTwo: "",
+  roundTwoGameFourTeamOne: "",
+  roundTwoGameFourTeamTwo: "",
+  roundTwoGameFiveTeamOne: "",
+  roundTwoGameFiveTeamTwo: "",
+  roundTwoGameSixTeamOne: "",
+  roundTwoGameSixTeamTwo: "",
+  roundTwoGameSevenTeamOne: "",
+  roundTwoGameSevenTeamTwo: "",
+  roundTwoGameEightTeamOne: "",
+  roundTwoGameEightTeamTwo: "",
+  roundTwoGameNineTeamOne: "",
+  roundTwoGameNineTeamTwo: "",
+  roundTwoGameTenTeamOne: "",
+  roundTwoGameTenTeamTwo: "",
+  roundTwoGameElevenTeamOne: "",
+  roundTwoGameElevenTeamTwo: "",
+  roundTwoGameTwelveTeamOne: "",
+  roundTwoGameTwelveTeamTwo: "",
+  roundTwoGameThirteenTeamOne: "",
+  roundTwoGameThirteenTeamTwo: "",
+  roundTwoGameFourteenTeamOne: "",
+  roundTwoGameFourteenTeamTwo: "",
+  roundTwoGameFifteenTeamOne: "",
+  roundTwoGameFifteenTeamTwo: "",
+  roundTwoGameSixteenTeamOne: "",
+  roundTwoGameSixteenTeamTwo: "",
   actions: {
-    pickWinner: function(){
-      this.get('matchup');
-    }
+    pickRoundTwoGameOneTeamOne: function(teamName){
+      var team = this.get('model');
+      console.log(team);
+      console.log(teamName);
+      this.set('roundTwoGameOneTeamOne', teamName);
+    },
+    pickRoundTwoGameOneTeamTwo: function(teamName){
+      var team = this.get('model');
+      console.log(team);
+      console.log(teamName);
+      this.set('roundTwoGameOneTeamTwo', teamName);
+    },
   }
 });
--- app/routes/bracket.js
+++ app/routes/bracket.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   renderTemplate: function(){
     this.render({ outlet: 'bracket'});
+  },
+  model: function(){
+    return this.store.find('team');
   }
 });
